97b216d97102f17548f913241175062b4207f7cc
Elamathy S <elamathy2000@gmail.com>
2020-07-29 21:45:13 +0530

    resolving issue

--- server.js
+++ server.js
@@ -22,6 +22,13 @@ const app =express();
 // app.use(bodyParser.json());
 app.use(cors());
 
+app.use(function(req,res,next){
+	res.setHeader('Access-Control-Allow-Origin','*');
+	res.setHeader('Access-Control-Allow-Methods','GET,POST,OPTIONS,PUT,PATCH,DELETE');
+	res.setHeader('Access-Control-Allow-Headers','X-Requested-With,content-type');
+	next();
+});
+
 app.get('/', (req,res) => { res.send('it is working') })
 
 app.post('/signin', (req,res) => {signin.handleSignin(req,res,db,bcrypt)});
