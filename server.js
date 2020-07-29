56615b3ddf48cc8b6eab4aef1abbe484f84ff0b1
Elamathy S <elamathy2000@gmail.com>
2020-07-29 22:30:59 +0530

    changing

--- server.js
+++ server.js
@@ -22,12 +22,12 @@ const app =express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.use(function(req,res,next){
-	res.setHeader('Access-Control-Allow-Origin','*');
-	res.setHeader('Access-Control-Allow-Methods','GET,POST,OPTIONS,PUT,PATCH,DELETE');
-	res.setHeader('Access-Control-Allow-Headers','X-Requested-With,content-type');
-	next();
-});
+// app.use(function(req,res,next){
+// 	res.setHeader('Access-Control-Allow-Origin','*');
+// 	res.setHeader('Access-Control-Allow-Methods','GET,POST,OPTIONS,PUT,PATCH,DELETE');
+// 	res.setHeader('Access-Control-Allow-Headers','X-Requested-With,content-type');
+// 	next();
+// });
 
 app.get('/', (req,res) => { res.send('it is working') })
 
