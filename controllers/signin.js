934a0a577876f83dc506bca5127b582cd021f12b
Elamathy S <elamathy2000@gmail.com>
2020-07-29 22:21:32 +0530

    changing

--- controllers/signin.js
+++ controllers/signin.js
@@ -6,19 +6,19 @@ const handleSignin = (req,res,db,bcrypt) =>{
 	db.select('email', 'hash').from('login')
 		.where('email','=',email)
 		.then(data =>{
-			const isValid = bcrypt.compareSync(req.body.password,data[0].hash)
+			const isValid = bcrypt.compareSync(password,data[0].hash)
 			if(isValid) {
 				return db.select('*').from('users')
-				  .where('email','=',req.body.email)
+				  .where('email','=',email)
 				  .then(user=>{
 				  	res.json(user[0])
 				  })
 				  .catch(err => res.status(400).json('unable to join'))
 			} else {
-				res.status(400).json('wrong credentials');
+				res.status(400).json('wrong credentials')
 			}
 		})
-		.catch(err => res.status(400).json('wrong credential'));
+		.catch(err => res.status(400).json('wrong credential'))
 }
 
 module.exports = {
