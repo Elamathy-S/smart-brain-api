4d7a6c8ccbe41339aa8506f05260499ac786a2e2
Elamathy S <elamathy2000@gmail.com>
2020-07-29 22:33:19 +0530

    changing

--- server.js
+++ server.js
@@ -18,16 +18,17 @@ const db = knex({
 });
 
 const app =express();
-
-app.use(bodyParser.json());
 app.use(cors());
 
-// app.use(function(req,res,next){
-// 	res.setHeader('Access-Control-Allow-Origin','*');
-// 	res.setHeader('Access-Control-Allow-Methods','GET,POST,OPTIONS,PUT,PATCH,DELETE');
-// 	res.setHeader('Access-Control-Allow-Headers','X-Requested-With,content-type');
-// 	next();
-// });
+app.use(bodyParser.json());
+
+
+app.use(function(req,res,next){
+	res.setHeader('Access-Control-Allow-Origin','*');
+	res.setHeader('Access-Control-Allow-Methods','GET,POST,OPTIONS,PUT,PATCH,DELETE');
+	res.setHeader('Access-Control-Allow-Headers','X-Requested-With,content-type');
+	next();
+});
 
 app.get('/', (req,res) => { res.send('it is working') })
 
